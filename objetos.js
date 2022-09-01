1688a57ee5936ac3bfa9c184326962c9b473bb4d
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2022-08-31 22:35:40 -0300

    objeto cliente

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listasCPFs = ['464986546847', '4846464646', '46498464646'];
+
+const cliente = ['nome', 'André', 'idade', 36];
+
+const cliente = {
+    nome: 'André',
+    idade: 36,
+    cpf = '6565465465',
+    email: 'andre@gmail.com'
+}
